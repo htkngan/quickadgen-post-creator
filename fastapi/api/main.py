bda3986f49d28fadbb94ff57fc608c96efe209e2
huynhngan <huynhthikimngan.cdc@gmail.com>
2025-04-24 11:30:23 +0700

    update from google.generativeai import Client

--- fastapi/api/main.py
+++ fastapi/api/main.py
@@ -2,7 +2,7 @@ from typing import Union
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
-from google import genai
+from google.generativeai import Client
 from dotenv import load_dotenv
 from fastapi.middleware.cors import CORSMiddleware
 import logging
@@ -25,7 +25,7 @@ import os
 # Load environment variables
 load_dotenv()
 
-client_gemini = genai.Client()
+client_gemini = Client()
 client_gpt = OpenAI()
 
 #gpt_model = os.getenv("gpt_model")
