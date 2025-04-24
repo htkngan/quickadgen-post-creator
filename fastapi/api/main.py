2f09ba5473b5391b1f64edba5d9dd991aea2eab8
huynhngan <huynhthikimngan.cdc@gmail.com>
2025-04-24 13:28:38 +0700

    change port 10000

--- fastapi/api/main.py
+++ fastapi/api/main.py
@@ -221,4 +221,4 @@ def update_item(item_id: int, item: Item):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
